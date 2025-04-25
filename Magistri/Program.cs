b47097a2634d46aadc5339ff7da4cd4950cdcd4f
Jakub Černý <AsKronak@seznam.cz>
2025-04-25 15:16:58 +0200

    fix hostingu

--- Magistri/Program.cs
+++ Magistri/Program.cs
@@ -16,7 +16,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql => sql.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null)));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
